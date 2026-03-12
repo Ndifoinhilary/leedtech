47e3d40020144ecdcaa0cf98986c08395372a516
ndifoinhilary <ndifoinhilary@gmail.com>
2026-03-12 10:53:18 +0100

    init LeedTech project with Maven

---
+++ src/main/java/com/hilary/leedtech/LeedTechApplication.java
@@ -0,0 +1,13 @@
+package com.hilary.leedtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeedTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeedTechApplication.class, args);
+    }
+
+}
