9cc8b7932a931a45ad536f2aa6b9d2abf35357eb
ndifoinhilary <ndifoinhilary@gmail.com>
2026-03-15 11:55:05 +0100

    update structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { FeePaymentPageComponent } from './fee-payment-page.component';
+import { FeePaymentPageComponent } from './fee-payment/fee-payment-page.component';
 
 export const routes: Routes = [
   { path: '', component: FeePaymentPageComponent },
